3c34b2be07e83d1fe28bdc804db228548b770b4c
Ritik Rai <96814171+ritkrai2111@users.noreply.github.com>
2023-12-31 12:13:26 +0000

    container config done

---
+++ chapter1.js
@@ -0,0 +1,8 @@
+//difference between var,const and let
+//variable names should be in camel case
+const accountId=212001;
+let accountName="Ritik Rai";
+var college="NSUT";
+//don't use var because of block scope and functional scope
+//to print multiple variables at once
+console.table([accountId,college]);

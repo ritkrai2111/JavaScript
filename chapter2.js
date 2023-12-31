03c6b711c60146fe08cc1607db7936c6c7b54ce3
Ritik Rai <96814171+ritkrai2111@users.noreply.github.com>
2023-12-31 13:02:25 +0000

    container config done

---
+++ chapter2.js
@@ -0,0 +1,9 @@
+//datatypes
+let name="ritik rai";
+let age=22;
+let improvement=true;
+//null is not undefined
+//to check data type of variables
+console.log(typeof name);
+//null has object datatype
+console.log(typeof null);

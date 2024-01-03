3fcc104d1996f07efa6cd7b328f4a943763832d3
Ritik Rai <96814171+ritkrai2111@users.noreply.github.com>
2024-01-03 13:54:34 +0000

    container config done

---
+++ chapter3.js
@@ -0,0 +1,10 @@
+console.log(2+3*4%7);
+//primitive vs non primitve or reference data types
+//primitve are call by value or basically u are given a copy of that data
+//primitve example string,null,number ,boolean,symbol,bigint
+
+//non primitive or reference are call by reference to the main data
+//examples are array and function
+//js is dynamically typed
+
+//stack memory for primitive types and heap for non primitive

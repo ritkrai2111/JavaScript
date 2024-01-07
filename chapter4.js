105226ad60fae1770af0d4e4d4f983309d61318b
Ritik Rai <96814171+ritkrai2111@users.noreply.github.com>
2024-01-07 11:09:49 +0000

    container config done

---
+++ chapter4.js
@@ -0,0 +1,9 @@
+//strings
+//how to add multiple strings
+const name="ritik rai";
+const age=22;
+console.log("hello my name is "+ name + ", I am " + age +"years old" );
+//the better way to do it
+console.log(`hello my name is ${name} and I am ${age} years old`);
+//so we can use ${variable_name} and use it all along with our text
+console.log(name.length);
